389dc8e7cc4f6a5941ecbc536b5c624cd778a450
DeMesaTech <hassandm16@gmail.com>
2026-09-14 18:40:46 +0800

    feat: add admin dashboard and account management pages

--- backend/create_account.py
+++ backend/create_account.py
@@ -1,8 +1,8 @@
 from utils import hash_password
 
-password = hash_password("Capstone.26")
+password = hash_password("gmvccAdmin.26")
 print(password)
 '''INSERT INTO account (email, name, password, role)
                VALUES (%s, %s, %s, %s, %s)
                RETURNING user_id'''
-print(f"INSERT INTO \"user\" (email, name, password, role)\n VALUES (\'hassandm@gmail.com\', \'Dan DM\', \'{password}\', \'student\');")
+print(f"INSERT INTO \"account\" (email, name, password, role)\n VALUES (\'admin@gmvcc.edu\', \'GMVCC Admin\', \'{password}\', \'admin\');")
